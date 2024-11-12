6fcd58de2b679cb4213e07a10ea36585e7e8bd2e
Esteban Vega <iestebanvega1@gmail.com>
2024-11-12 12:00:55 -0500

    create new use case to save files

--- 04-multiplication/src/presentation/server-app.ts
+++ 04-multiplication/src/presentation/server-app.ts
@@ -1,4 +1,5 @@
 import { CreateTable } from "../domain/use-cases/create-table.use-case";
+import { SaveFile } from "../domain/use-cases/save-file.use-case";
 interface RunOptions {
   base: number;
   limit: number;
@@ -10,6 +11,14 @@ export class ServerApp {
     console.log("Server running....");
     const table = new CreateTable().execute({ base, limit });
 
+    const wasTable = new SaveFile().execute({
+      fileContent: table,
+      fileDestination: `outputs/table-${base}`,
+    });
+
     showTable && console.log(table);
+    wasTable
+      ? console.log("File created!!")
+      : console.log("File not created!!");
   }
 }
