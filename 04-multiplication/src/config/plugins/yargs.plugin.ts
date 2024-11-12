97a03f1384248dc1d6de5e88cc7107baf3ec8aff
Esteban Vega <iestebanvega1@gmail.com>
2024-11-12 10:49:53 -0500

    add validation in yargs

--- 04-multiplication/src/config/plugins/yargs.plugin.ts
+++ 04-multiplication/src/config/plugins/yargs.plugin.ts
@@ -20,4 +20,8 @@ export const yarg = yargs(hideBin(process.argv))
     default: false,
     describe: "Show multiplication table",
   })
+  .check((argv) => {
+    if (argv.b < 1) throw "Error: base must be greater than 0";
+    return true;
+  })
   .parseSync();
